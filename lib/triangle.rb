7874c79d8d0867935c42bde641f32ff612a83aa6
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-31 11:28:53 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_accessor :length, :width, :hypotenuse
+
+  def initialize (length, width, hypotenuse) 
+    @length = length
+    @width = width
+    @hypotenuse = hypotenuse
+  end
+
+  def kind 
+    validate_triangle
+      if (length == width && width == hypotenuse)
+          :equilateral
+        elsif (length == width || length == hypotenuse || hypotenuse == width)
+          :isosceles
+        else
+          :scalene
+      end
+  end 
+
+  def validate_triangle
+    real_triangle = [(length + width > hypotenuse), (length + hypotenuse > width), (width + hypotenuse > length)]
+    [length, width, hypotenuse].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+   
 end
